da744df2976b288c4339e2e8f8e591f6b9e98b29
haodam@addbee.xyz <haodam@addbee.xyz>
2021-12-03 09:31:13 +0000

    added teget.name variations

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -1,13 +1,17 @@
 {% macro generate_database_name(custom_database_name=none, node=none) -%}
 
     {%- set default_database = target.database -%}
-    {%- if custom_database_name is none -%}
+    {%- if ((custom_database_name is none) AND (target.name <> 'QA')) -%}
 
         {{ default_database }}
 
-    {%- elif target.name == 'QA' -%}
+    {%- elif ((target.name == 'QA') AND (custom_database_name is none)) -%}
 
-        {{ custom_database_name | trim }}_QA
+        {{ default_database }}_QA
+
+    {%- elif ((target.name == 'QA') AND (custom_database_name is NOT none)) -%}
+
+        {{ custom_database_name | trim }}_QA    
 
     {%- else -%}
 
